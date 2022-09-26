360f4a019438cd1c7a29620f7639c80ccb9b206c
Mischka Booley <mischka@gearbox.com.au>
2022-09-26 12:08:50 +1000

    fix start function

--- lib/bobsburgers/cli.rb
+++ lib/bobsburgers/cli.rb
@@ -6,28 +6,31 @@ class CLI
     start
   end
 
- def start
-      input = nil
-      display_characters
-      while input != "q" 
-        input = gets.strip
-        if input.to_i > 0 && input.to_i < 507
-          Character.list_character_details(input)
-          options
-        elsif input == "b"
-          display_characters
-        elsif input == "q"
-          goodbye
-        else
+  def start
+    input = nil
+    display_characters
+    while input != "q"
+      input = gets.strip
+      if input.to_i > 0 && input.to_i < 507
+        Character.list_character_details(input)
+        options
+      elsif input == "b"
+        puts "\nBob's Burgers Characters:"
+        puts ""
+        display_characters
+      elsif input == "q"
+        goodbye
+      else
         puts "Invalid entry, please enter your choice again, enter 'b' to see the character list or enter 'q' to quit."
-        end
       end
     end
+  end
 
   def display_characters
     Character.list_all_characters
     puts "\nPlease enter the number for the character you'd like details on or enter 'q' to quit: "
   end
+
 
   def options
     puts "\nEnter a new character number, type 'b' to return to the list of characters or 'q' to quit:"
